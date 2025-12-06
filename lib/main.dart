0b493aa25b5afa305c4b8cd8849c469955f0956a
Farhan Febryan <nefarha1@gmail.com>
2025-12-06 18:18:02 +0700

    refactor: removed unused package

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:get_storage/get_storage.dart';
 import 'package:hive_flutter/adapters.dart';
 
 import 'package:special_math_quizz/quizz_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await GetStorage.init('special_math_quizz_cache');
   await Hive.initFlutter();
   await Hive.openBox<List<int>>('answered');
   runApp(
